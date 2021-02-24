3201e5db3b1789af3d95eb645cbbc9a0608f00c8
Spencer Christian <s.christian141@gmail.com>
2021-02-24 11:10:16 -0500

    GenerateList

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-import "./styles.css";
+const locationListTarget = document.querySelector(".locationList");
+const listOfLocations = {
+  NewYork: {
+    name: "New York, NY",
+    ID: 1234,
+    TimeZone: "EST",
+    availability: 80
+  },
+  SanDiego: {
+    name: "Sand Diego, CA",
+    ID: 321,
+    TimeZone: "PST",
+
+    availability: 20
+  },
+  Texas: {
+    name: "Texas",
+    ID: 654,
+    TimeZone: "CST",
+    availability: 100
+  }
+};
+function createEle(tagName, styleSheet, dataContent) {
+  let tempObject = document.createElement(`${tagName}`);
+  tempObject.classList.add(`${styleSheet}`);
+  tempObject.innerText = dataContent;
+  return tempObject;
+}
+
+// let location = createElement("DIV", "locations", "Content");
+
+function generateList() {
+  const locationKeys = Object.keys(listOfLocations);
+  for (let i = 0; i < locationKeys.length; i++) {
+    let objKey = Object.keys(listOfLocations)[i];
+    let div_ele = createEle("div", "cityTile2", " ");
+    let h1_ele = createEle("h1", "cityTitle", listOfLocations[objKey].name);
+    let p_ele = createEle(
+      "p",
+      "cityStatus",
+      `Availability: ${listOfLocations[objKey].availability}%`
+    );
+    locationListTarget.appendChild(div_ele);
+    div_ele.appendChild(h1_ele);
+    div_ele.appendChild(p_ele);
+  }
+}
+window.addEventListener("DOMContentLoaded", generateList());
